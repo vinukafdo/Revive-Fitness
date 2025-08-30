708b9ea210aede0ab370923cef620a93348e7eed
vinukafdo <vinuka.fernando@gmail.com>
2025-08-30 23:17:55 +0400

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-brand">
+      <h1 className="text-5xl font-bold">Welcome to Revive Fitness</h1>
+      <p className="mt-4 text-lg text-white">Your journey to strength begins here.</p>
+    </div>
+  );
+}
